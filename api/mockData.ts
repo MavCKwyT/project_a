6a2eecab34b316c38ec80fec34b8c47361a64558
evgenii <evgenklochkov163@gmail.com>
2024-09-03 12:54:40 +0400

    initial commit

---
+++ api/mockData.ts
@@ -0,0 +1,302 @@
+const mockData = [
+  {
+    price: 45480,
+    carrier: "LH",
+    segments: [
+      {
+        origin: "HND",
+        destination: "JFK",
+        date: "2025-03-18T11:59:56",
+        stops: ["LAX"],
+        duration: 78,
+      },
+      {
+        origin: "HND",
+        destination: "CDG",
+        date: "2024-10-02T11:59:56",
+        stops: [],
+        duration: 330,
+      },
+    ],
+  },
+  {
+    price: 34030,
+    carrier: "BA",
+    segments: [
+      {
+        origin: "LED",
+        destination: "AMS",
+        date: "2025-08-18T11:59:56",
+        stops: ["CDG", "SIN"],
+        duration: 520,
+      },
+      {
+        origin: "DXB",
+        destination: "HND",
+        date: "2025-01-28T11:59:56",
+        stops: ["LAX", "AMS"],
+        duration: 139,
+      },
+    ],
+  },
+  {
+    price: 37534,
+    carrier: "BA",
+    segments: [
+      {
+        origin: "LAX",
+        destination: "LED",
+        date: "2025-06-01T11:59:56",
+        stops: [],
+        duration: 616,
+      },
+      {
+        origin: "AMS",
+        destination: "FRA",
+        date: "2024-10-16T11:59:56",
+        stops: ["JFK", "CDG", "AMS"],
+        duration: 569,
+      },
+    ],
+  },
+  {
+    price: 30121,
+    carrier: "SQ",
+    segments: [
+      {
+        origin: "SIN",
+        destination: "JFK",
+        date: "2025-01-02T11:59:56",
+        stops: ["LED", "CDG", "JFK"],
+        duration: 432,
+      },
+      {
+        origin: "LED",
+        destination: "FRA",
+        date: "2025-05-08T11:59:56",
+        stops: ["DXB", "SIN"],
+        duration: 461,
+      },
+    ],
+  },
+  {
+    price: 13675,
+    carrier: "BA",
+    segments: [
+      {
+        origin: "LAX",
+        destination: "SVO",
+        date: "2025-01-01T11:59:56",
+        stops: ["AMS"],
+        duration: 589,
+      },
+      {
+        origin: "HND",
+        destination: "LAX",
+        date: "2024-09-08T11:59:56",
+        stops: ["DXB", "SIN"],
+        duration: 603,
+      },
+    ],
+  },
+  {
+    price: 19133,
+    carrier: "LH",
+    segments: [
+      {
+        origin: "SVO",
+        destination: "LED",
+        date: "2025-05-23T11:59:56",
+        stops: ["JFK", "DXB"],
+        duration: 573,
+      },
+      {
+        origin: "FRA",
+        destination: "DXB",
+        date: "2024-10-31T11:59:56",
+        stops: ["JFK"],
+        duration: 569,
+      },
+    ],
+  },
+  {
+    price: 45260,
+    carrier: "JL",
+    segments: [
+      {
+        origin: "SVO",
+        destination: "HND",
+        date: "2024-09-11T11:59:56",
+        stops: [],
+        duration: 332,
+      },
+      {
+        origin: "CDG",
+        destination: "DXB",
+        date: "2025-02-18T11:59:56",
+        stops: ["CDG"],
+        duration: 602,
+      },
+    ],
+  },
+  {
+    price: 23470,
+    carrier: "LH",
+    segments: [
+      {
+        origin: "LAX",
+        destination: "FRA",
+        date: "2025-05-20T11:59:56",
+        stops: ["SIN"],
+        duration: 417,
+      },
+      {
+        origin: "HND",
+        destination: "AMS",
+        date: "2024-10-12T11:59:56",
+        stops: [],
+        duration: 75,
+      },
+    ],
+  },
+  {
+    price: 33841,
+    carrier: "KL",
+    segments: [
+      {
+        origin: "JFK",
+        destination: "LED",
+        date: "2024-10-08T11:59:56",
+        stops: [],
+        duration: 392,
+      },
+      {
+        origin: "LED",
+        destination: "LAX",
+        date: "2025-04-03T11:59:56",
+        stops: [],
+        duration: 347,
+      },
+    ],
+  },
+  {
+    price: 17293,
+    carrier: "EK",
+    segments: [
+      {
+        origin: "CDG",
+        destination: "AMS",
+        date: "2024-12-16T11:59:56",
+        stops: ["JFK"],
+        duration: 543,
+      },
+      {
+        origin: "DXB",
+        destination: "FRA",
+        date: "2025-03-05T11:59:56",
+        stops: ["LAX", "LED"],
+        duration: 516,
+      },
+    ],
+  },
+  {
+    price: 39930,
+    carrier: "BA",
+    segments: [
+      {
+        origin: "SVO",
+        destination: "LAX",
+        date: "2025-04-25T11:59:56",
+        stops: [],
+        duration: 299,
+      },
+      {
+        origin: "AMS",
+        destination: "CDG",
+        date: "2024-09-06T11:59:56",
+        stops: ["LED", "DXB"],
+        duration: 208,
+      },
+    ],
+  },
+  {
+    price: 21547,
+    carrier: "AF",
+    segments: [
+      {
+        origin: "SIN",
+        destination: "CDG",
+        date: "2025-04-09T11:59:56",
+        stops: [],
+        duration: 122,
+      },
+      {
+        origin: "LAX",
+        destination: "JFK",
+        date: "2024-09-27T11:59:56",
+        stops: ["AMS"],
+        duration: 632,
+      },
+    ],
+  },
+  {
+    price: 21686,
+    carrier: "SU",
+    segments: [
+      {
+        origin: "AMS",
+        destination: "HND",
+        date: "2025-05-18T11:59:56",
+        stops: ["LAX"],
+        duration: 563,
+      },
+      {
+        origin: "SVO",
+        destination: "DXB",
+        date: "2025-01-05T11:59:56",
+        stops: ["JFK", "AMS"],
+        duration: 551,
+      },
+    ],
+  },
+  {
+    price: 45848,
+    carrier: "AF",
+    segments: [
+      {
+        origin: "HND",
+        destination: "FRA",
+        date: "2024-11-19T11:59:56",
+        stops: ["AMS"],
+        duration: 111,
+      },
+      {
+        origin: "CDG",
+        destination: "LAX",
+        date: "2025-06-02T11:59:56",
+        stops: ["AMS"],
+        duration: 319,
+      },
+    ],
+  },
+  {
+    price: 28762,
+    carrier: "EK",
+    segments: [
+      {
+        origin: "JFK",
+        destination: "SVO",
+        date: "2024-11-21T11:59:56",
+        stops: ["CDG", "FRA"],
+        duration: 119,
+      },
+      {
+        origin: "LAX",
+        destination: "SIN",
+        date: "2024-11-24T11:59:56",
+        stops: ["LAX", "CDG", "HND"],
+        duration: 531,
+      },
+    ],
+  },
+];
